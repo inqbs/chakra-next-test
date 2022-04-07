ab853f7b104a4df5b71dc7955d3448480a2f1c6f
inqbs <incubass64@gmail.com>
2022-04-07 16:42:49 +0900

    add: apply chakra-ui in wordle page

--- pages/wordle.tsx
+++ pages/wordle.tsx
@@ -1,21 +1,23 @@
+import { Box, Button, Center, Flex, Heading, HStack, PinInput, PinInputField, Text, VStack } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { useWordle, WORD_CORRECT_TYPE } from "../utils/wordle/wordle";
 
 const Wordle: NextPage = () => {
 
   const [isRunning, setIsRunning] = useState<boolean>(true)
   const [input, setInput] = useState<string>('')
+  const firstInput = useRef<HTMLInputElement>(null)
   const {count, history, checkWord} = useWordle()
 
-  const getStyle = (state: WORD_CORRECT_TYPE) => {
+  const getTileStyle = (state: WORD_CORRECT_TYPE) => {
     switch(state){
       case WORD_CORRECT_TYPE.CORRECT:
-        return {backgroundColor: '#66d134'}
+        return {backgroundColor: 'green.300', textColor: 'white'}
       case WORD_CORRECT_TYPE.PARTITIAL:
-        return {backgroundColor: '#d1c134'}
+        return {backgroundColor: 'yellow.300', textColor: 'white'}
       default:
-        return {backgroundColor: '#AAAAAA'}
+        return {backgroundColor: 'gray.300', textColor: 'gray.900'}
     }
   }
 
@@ -39,30 +41,40 @@ const Wordle: NextPage = () => {
       setIsRunning(false)
     }
 
-    //  clear input
     setInput('')
+    firstInput?.current?.focus?.()
   }
 
   return (
-    <div>
-      <h1>wordle</h1>
-      <h3>count: {count}</h3>
-      <ul>
+    <VStack gap={4}>
+      <Box textAlign="center">
+        <Heading>wordle</Heading>
+        <Text>count: {count}</Text>
+      </Box>
+      <VStack spacing={4}>
         {history.map((word, index) => (
-          <li key={`history-${index}`}>
-            {word.map((char, charIndex) => (
-              <div key={`char-${index}-${charIndex}`} style={getStyle(char.state)}>
-                {char.word}
-              </div>
-           ))}
-          </li>
+            <HStack  key={`history-${index}`}>
+              {word.map((char, charIndex) => (
+                <Center key={`char-${index}-${charIndex}`} w={10} h={10} {...getTileStyle(char.state)}>
+                  {char.word}
+                </Center>
+            ))}
+            </HStack>
         ))}
-      </ul>
+      </VStack>
       <form onSubmit={submit}>
-        <input type="text" value={input} onChange={e => setInput(e.target.value)} />
-        <button disabled={!isRunning}>check</button>
+        <Center>
+          <Flex gap={4}>
+            <HStack flex="1">
+              <PinInput type="alphanumeric" variant="flushed" value={input} onChange={setInput}>
+                {Array(5).fill(undefined).map((_,idx) => (<PinInputField key={`input-${idx}`} {...(idx === 0 ? { ref:firstInput} : {})} />))}
+              </PinInput>
+            </HStack>
+            <Button type="submit" colorScheme="blue" disabled={!isRunning}>check</Button>
+          </Flex>
+        </Center>
       </form>
-    </div>
+    </VStack>
   )
 }
 export default Wordle
