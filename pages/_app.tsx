0ea4f0a5d4bd60ae4f5ceb68ee90822b52394a34
inqbs <incubass64@gmail.com>
2022-04-08 17:12:07 +0900

    chore: move layout's component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import Layout from '../components/layout/index'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
